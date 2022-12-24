840c6507a16dc48e3730aac03ed222c385dd529a
Ashu <ashu.as222@gmail.com>
2022-12-24 12:58:36 +0530

    file has been commmited

---
+++ controller/users_controller.js
@@ -0,0 +1,81 @@
+const User = require('../models/user');
+
+module.exports.profile = function(req, res){
+     if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('user_profile',{
+                    title: "User Profile",
+                    user: user
+                })
+            }
+            return res.redirect('./users/sign-in'); 
+        });
+     }
+     else{
+        return res.redirect('/users/sign-in');
+     }
+
+    // return res.render('user_profile',{
+    //     title: 'User profile'
+    // })
+}
+
+// render sign up
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title : "codiel signup"
+    })
+}
+
+//render sign in
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title : "codiel signin"
+    })
+}
+//get the sign up data
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }
+
+        else{
+            return res.redirect('back');
+        }
+    })
+}
+//sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    //steps authenticate
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user while signing in'); return}
+
+        // handle user found
+        if(user){
+            //handle password which doesn't match
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            //hanlde session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    })
+
+}
