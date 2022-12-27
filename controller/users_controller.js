50410b0d1acd625ec0d789902394d30010e79d0e
Ashu <ashu.as222@gmail.com>
2022-12-27 13:21:32 +0530

    as

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,81 +1,150 @@
 const User = require('../models/user');
 
+
 module.exports.profile = function(req, res){
-     if(req.cookies.user_id){
-        User.findById(req.cookies.user_id, function(err, user){
-            if(user){
-                return res.render('user_profile',{
-                    title: "User Profile",
-                    user: user
-                })
-            }
-            return res.redirect('./users/sign-in'); 
-        });
-     }
-     else{
-        return res.redirect('/users/sign-in');
-     }
-
-    // return res.render('user_profile',{
-    //     title: 'User profile'
-    // })
+    console.log("hello im in profile");
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
 }
 
-// render sign up
+
+// render the sign up page
 module.exports.signUp = function(req, res){
-    return res.render('user_sign_up',{
-        title : "codiel signup"
+    
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
     })
 }
 
-//render sign in
+
+// render the sign in page
 module.exports.signIn = function(req, res){
-    return res.render('user_sign_in',{
-        title : "codiel signin"
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
     })
 }
-//get the sign up data
+
+// get the sign up data
 module.exports.create = function(req, res){
-    if(req.body.password != req.body.confirm_password){
+    if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
+
     User.findOne({email: req.body.email}, function(err, user){
         if(err){console.log('error in finding user in signing up'); return}
 
-        if(!user){
+        if (!user){
             User.create(req.body, function(err, user){
                 if(err){console.log('error in creating user while signing up'); return}
 
                 return res.redirect('/users/sign-in');
             })
-        }
-
-        else{
+        }else{
             return res.redirect('back');
         }
-    })
+
+    });
 }
-//sign in and create a session for the user
+
+
+// sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //steps authenticate
-    //find the user
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user while signing in'); return}
-
-        // handle user found
-        if(user){
-            //handle password which doesn't match
-            if(user.password != req.body.password){
-                return res.redirect('back');
-            }
-            //hanlde session creation
-            res.cookie('user_id', user.id);
-            return res.redirect('/users/profile');
-        }
-        else{
-            //handle user not found
-            return res.redirect('back');
-        }
-    })
-
+    return res.redirect('/');
 }
+
+
+
+
+
+// const User = require('../models/user');
+
+// module.exports.profile = function(req, res){
+//     //  if(req.cookies.user_id){
+//     //     User.findById(req.cookies.user_id, function(err, user){
+//     //         if(user){
+//     //             return res.render('user_profile',{
+//     //                 title: "User Profile",
+//     //                 user: user
+//     //             })
+//     //         }
+//     //         return res.redirect('./users/sign-in'); 
+//     //     });
+//     //  }
+//     //  else{
+//     //     return res.redirect('/users/sign-in');
+//     //  }
+
+//     return res.render('user_profile',{
+//         title: 'User profile'
+//     })
+// }
+
+// // render sign up
+// module.exports.signUp = function(req, res){
+//     return res.render('user_sign_up',{
+//         title : "codiel signup"
+//     })
+// }
+
+// //render sign in
+// module.exports.signIn = function(req, res){
+//     return res.render('user_sign_in',{
+//         title : "codiel signin"
+//     })
+// }
+// //get the sign up data
+// module.exports.create = function(req, res){
+//     if(req.body.password != req.body.confirm_password){
+//         return res.redirect('back');
+//     }
+//     User.findOne({email: req.body.email}, function(err, user){
+//         if(err){console.log('error in finding user in signing up'); return}
+
+//         if(!user){
+//             User.create(req.body, function(err, user){
+//                 if(err){console.log('error in creating user while signing up'); return}
+
+//                 return res.redirect('/users/sign-in');
+//             })
+//         }
+
+//         else{
+//             return res.redirect('back');
+//         }
+//     })
+// }
+// //sign in and create a session for the user
+
+// //using passport.js
+// module.exports.createSession = function(req, res){
+//     return res.redirect('/');
+// }
+
+
+
+// //manual authentication -->
+
+// // module.exports.createSession = function(req, res){
+// //     //steps authenticate
+// //     //find the user
+// //     User.findOne({email: req.body.email}, function(err, user){
+// //         if(err){console.log('error in finding user while signing in'); return}
+
+// //         // handle user found
+// //         if(user){
+// //             //handle password which doesn't match
+// //             if(user.password != req.body.password){
+// //                 return res.redirect('back');
+// //             }
+// //             //hanlde session creation
+// //             res.cookie('user_id', user.id);
+// //             return res.redirect('/users/profile');
+// //         }
+// //         else{
+// //             //handle user not found
+// //             return res.redirect('back');
+// //         }
+// //     })
+
+// // }
--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 const express = require('express');
 const port = 3000;
 const cookieParser = require('cookie-parser');
+const sassMiddleware = require('node-sass');
 const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.urlencoded());
 app.use(cookieParser());
+app.use(express.static('./assets'));
+
 //setup database
 
 mongoose.connect("mongodb://0.0.0.0:27017/Prjoect2");
 
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
-
 db.once('open', function(){
     console.log('Connected to Database :: MongoDB');
 });
+
+
+
+//used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+
+
+const MongoStore = require('connect-mongo');
+
+
+// mongo store is used to store the session cookie in the db
+app.use(session({
+    name: 'codial',
+    secret: 'something',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100 )
+    },
+    // store: new MongoStore({
+    //         mongooseConnection: db,
+    //         autoRemove: 'disable'
+    //     },
+    //     function(err){
+    //         console.log(err || 'connect-mongo setup ok');
+    //     }
+    // )
+}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(passport.setAuthenticatedUser);
 
 //use express router
 const way = require('./routes/index');
@@ -27,6 +61,8 @@ app.use('/', way);
 //setup the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
 
 app.listen(port, function(err){
     if(err) console.log('error in server');
